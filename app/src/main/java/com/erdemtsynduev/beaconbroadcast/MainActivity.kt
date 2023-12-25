e33ca43875b007143a62270c1aef05535739f57b
Erdem Tsynduev <erdemtsynduev@gmail.com>
2023-12-25 19:12:24 +0300

    FEAT: Create simple iBeacon

--- app/src/main/java/com/erdemtsynduev/beaconbroadcast/MainActivity.kt
+++ app/src/main/java/com/erdemtsynduev/beaconbroadcast/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.erdemtsynduev.beaconbroadcast
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.erdemtsynduev.beaconbroadcast.ui.theme.BeaconBroadcastTheme
+import org.altbeacon.beacon.Beacon
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.BeaconTransmitter
+import java.util.Arrays
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             BeaconBroadcastTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,10 +29,30 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    startAdvertising(applicationContext)
                 }
             }
         }
     }
+}
+
+const val RADIUS_NETWORK_MANUFACTURER = 0x0118
+const val DEFAULT_TX_POWER = -59
+
+private fun startAdvertising(context: Context) {
+    val beacon = Beacon.Builder()
+        .setBluetoothName("Test")
+        .setId1("C467D3B6-8102-FE9E-161D-478ED75D19BE")
+        .setId2("1")
+        .setId3("2")
+        .setManufacturer(RADIUS_NETWORK_MANUFACTURER)
+        .setDataFields(listOf(0L))
+        .setTxPower(DEFAULT_TX_POWER)
+        .build()
+    val beaconParser =
+        BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+    val beaconTransmitter = BeaconTransmitter(context, beaconParser)
+    beaconTransmitter.startAdvertising(beacon)
 }
 
 @Composable
